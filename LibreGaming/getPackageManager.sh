afa6392fa77797bf73c103472eb66a3cecfa4e0b
Ahmed Al Balochi <a7mad98.work@gmail.com>
2021-09-09 18:30:39 +0500

    initial support for openSUSE

--- LibreGaming/getPackageManager.sh
+++ LibreGaming/getPackageManager.sh
@@ -4,4 +4,5 @@ elif [ -x "$(command -v yay)" ]; then echo "yay"
 elif [ -x "$(command -v paru)" ]; then echo "paru"
 elif [ -x "$(command -v pacman)" ]; then echo "pacman"
 elif [ -x "$(command -v apt)" ]; then echo "apt"
+elif [ -x "$(command -v zypper)" ]; then echo "zypper"
 else echo "FAILED TO INSTALL PACKAGE: Package manager not found."; fi

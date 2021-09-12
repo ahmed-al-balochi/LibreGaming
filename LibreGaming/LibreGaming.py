1813cebd70c192818ab2aa4071a042af4f88c3d9
Ahmed Al Balochi <a7mad98.work@gmail.com>
2021-09-13 02:46:00 +0500

    minor change to SUSE

--- LibreGaming/LibreGaming.py
+++ LibreGaming/LibreGaming.py
@@ -14,6 +14,7 @@ rootScript = os.path.join(dir, 'getRoot.sh') # get the path to the root script
 global rootCommand
 rootCommand = subprocess.getoutput("sh "+rootScript)      # gets the rootCommand like sudo doas if both dont exist it will fall back to su -
 
+
 def installPkgs():
     if PackageManager == distro[0]:  #packages for Ubuntu and Ubuntu based distros
         print("\nNow installing Ubuntu Gaming Packages")
@@ -57,7 +58,7 @@ def installPkgs():
         "chmod +x winetricks",
         rootCommand + " mv winetricks /usr/local/bin/",
         rootCommand + " dnf copr enable atim/heroic-games-launcher -y",
-        rootCommand + " dnf update",
+        rootCommand + " dnf update -y",
         rootCommand + " dnf install python3-pip heroic-games-launcher-bin wine lutris gamemode goverlay steam alsa-plugins-pulseaudio.i686 glibc-devel.i686 glibc-devel libgcc.i686 libX11-devel.i686 freetype-devel.i686 libXcursor-devel.i686 libXi-devel.i686 libXext-devel.i686 libXxf86vm-devel.i686 libXrandr-devel.i686 libXinerama-devel.i686 mesa-libGLU-devel.i686 mesa-libOSMesa-devel.i686 libXrender-devel.i686 libpcap-devel.i686 ncurses-devel.i686 libzip-devel.i686 lcms2-devel.i686 zlib-devel.i686 libv4l-devel.i686 libgphoto2-devel.i686 cups-devel.i686 libxml2-devel.i686 openldap-devel.i686 libxslt-devel.i686 gnutls-devel.i686 libpng-devel.i686 flac-libs.i686 json-c.i686 libICE.i686 libSM.i686 libXtst.i686 libasyncns.i686 liberation-narrow-fonts.noarch libieee1284.i686 libogg.i686 libsndfile.i686 libuuid.i686 libva.i686 libvorbis.i686 libwayland-client.i686 libwayland-server.i686 llvm-libs.i686 mesa-dri-drivers.i686 mesa-filesystem.i686 mesa-libEGL.i686 mesa-libgbm.i686 nss-mdns.i686 ocl-icd.i686 pulseaudio-libs.i686 sane-backends-libs.i686 tcp_wrappers-libs.i686 unixODBC.i686 samba-common-tools.x86_64 samba-libs.x86_64 samba-winbind.x86_64 samba-winbind-clients.x86_64 samba-winbind-modules.x86_64 mesa-libGL-devel.i686 fontconfig-devel.i686 libXcomposite-devel.i686 libtiff-devel.i686 openal-soft-devel.i686 mesa-libOpenCL-devel.i686 opencl-utils-devel.i686 alsa-lib-devel.i686 gsm-devel.i686 libjpeg-turbo-devel.i686 pulseaudio-libs-devel.i686 pulseaudio-libs-devel gtk3-devel.i686 libattr-devel.i686 libva-devel.i686 libexif-devel.i686 libexif.i686 glib2-devel.i686 mpg123-devel.i686 mpg123-devel.x86_64 libcom_err-devel.i686 libcom_err-devel.x86_64 libFAudio-devel.i686 libFAudio-devel.x86_64 -y"
         ]
         for i in Fedora:
@@ -65,6 +66,7 @@ def installPkgs():
     elif PackageManager == distro[5]:    #packages for OpenSUSE
         print("\nNow installing OpenSUSE Gaming Packages")
         OpenSUSE = rootCommand + " zypper install lutris steam wine-staging goverlay gamemode -y"  ##also include the update command       
+        os.system(rootCommand + " zypper update -y")
         os.system(OpenSUSE)
         print('Downloading Heroic latest AppImage')
         url = 'https://api.github.com/repos/Heroic-Games-Launcher/HeroicGamesLauncher/releases/latest'

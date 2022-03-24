a9bca9e1b55b7b69b9d86cd84f0803af3384cba8
Ahmed Al Balochi <a7mad98.work@gmail.com>
2022-03-24 23:43:20 +0500

    trying to fix not grabing Ubuntu release codename

--- LibreGaming/distro_pkgs/Ubuntu.py
+++ LibreGaming/distro_pkgs/Ubuntu.py
@@ -5,20 +5,22 @@ class Ubuntu:
     Here are all the pkgs that LibreGaming installs for Ubuntu
     """
 
-# Saves the Fedora release codename. For example: Fedora 35
+# Saves the Ubuntu release codename. For example: focal fossa 
     ReleaseCodename =  ""
 
 # For installing Basic Packages like Steam, Wine, Gamemode 
-    Ubuntu_Basics = [
+    def Ubuntu_Basics(self, ReleaseCodename):
+        basic_pkgs = [
         " dpkg --add-architecture i386",
         "wget -nc https://dl.winehq.org/wine-builds/winehq.key",
         " apt-key add winehq.key",
-        " add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ "+ ReleaseCodename +" main' -y",
+        " add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ "+ self.ReleaseCodename +" main' -y",
         " add-apt-repository multiverse -y",
         " apt update",
         " apt install --install-recommends winehq-staging -y",
         " apt install steam winetricks python3-pip gawk curl meson libsystemd-dev pkg-config ninja-build git libdbus-1-dev libinih-dev dbus-user-session libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 -y"
         ]
+        return basic_pkgs 
 
 # For installing Lutris
     Ubuntu_Lutris = [
@@ -45,7 +47,7 @@ class Ubuntu:
             if  i['name'].endswith('.deb'):
                 url= i['browser_download_url']
         wget.download(url, "heroic.deb")
-        os.system(" dpkg -i heroic.deb") #
+        os.system(" dpkg -i heroic.deb")
 
     def __init__(self):
         distroName = str(subprocess.getoutput("lsb_release -is"))

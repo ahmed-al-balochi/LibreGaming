98347af5b966134706a7f955f27a4dd360cfe17e
Ahmed Al Balochi <a7mad98.work@gmail.com>
2022-03-28 11:38:22 +0500

    fixed the problem with Arch_Lutris

--- LibreGaming/distro_pkgs/Arch.py
+++ LibreGaming/distro_pkgs/Arch.py
@@ -10,7 +10,7 @@ class Arch:
     Arch_Basics = " pacman -Syu python-pip wine-staging winetricks steam gamemode giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader -y --needed --noconfirm"
 
 # For installing Lutris
-    def Arch_Lutris():
+    def Arch_Lutris(self):
         Install_Lutris = self.PackageManager + " -S python-evdev lutris -y --needed --noconfirm"
 
     # The PackageManager variable uses the updated PackageManager only in functions. otherwise  its values will be blank

8eae81c3725bd731c0b132923da8bb6ef7a76be7
Ahmed Al Balochi <a7mad98.work@gmail.com>
2022-03-29 02:55:06 +0500

    removed the root and package manager shell scripts

--- LibreGaming/getPackageManager.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -x "$(command -v dnf)" ]; then echo "dnf"
-elif [ -x "$(command -v yay)" ]; then echo "yay"
-elif [ -x "$(command -v paru)" ]; then echo "paru"
-elif [ -x "$(command -v pacman)" ]; then echo "pacman"
-elif [ -x "$(command -v apt)" ]; then echo "apt"
-elif [ -x "$(command -v zypper)" ]; then echo "zypper"
-else echo "FAILED TO INSTALL PACKAGE: Package manager not found."; fi
--- LibreGaming/getRoot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-if  [ -x "$(command -v doas)" ]; then echo 'doas' 
-elif  [ -x "$(command -v sudo)" ]; then echo 'sudo'
-else echo "su -"; fi

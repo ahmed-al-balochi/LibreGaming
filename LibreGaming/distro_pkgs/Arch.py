d15c3a7f88105c1b939ae99e575e0d2cee3e4b0c
Ahmed Al Balochi <a7mad98.work@gmail.com>
2022-03-28 11:05:27 +0500

    added Arch functions

--- LibreGaming/distro_pkgs/Arch.py
+++ LibreGaming/distro_pkgs/Arch.py
@@ -7,20 +7,29 @@ class Arch:
     PackageManager = ""
 
 # For installing Basic Packages like Steam, Wine, Gamemode 
-    Arch_AUR_Basics = PackageManager + " -Syu python-pip wine-staging winetricks steam gamemode giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader -y --needed --noconfirm"
     Arch_Basics = " pacman -Syu python-pip wine-staging winetricks steam gamemode giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader -y --needed --noconfirm"
-
 # For installing Lutris
     Arch_Lutris = " pacman -S python-evdev lutris -y --needed --noconfirm"
 
+    # The PackageManager variable uses the updated PackageManager only in functions. otherwise  its values will be blank
+    def Arch_AUR_Basics(self)
+        AUR_Basics = PackageManager + " -Syu python-pip wine-staging winetricks steam gamemode giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader -y --needed --noconfirm"
+        return AUR_Basics
+
 # For installing Heroic
-    Arch_AUR_Heroic = PackageManager + " -S heroic-games-launcher-bin -y --needed --noconfirm"
+    def Arch_AUR_Heroic(self)
+        AUR_Heroic = PackageManager + " -S heroic-games-launcher-bin -y --needed --noconfirm"
+        return AUR_Heroic
 
 # For installing The FPS programs and overlays
-    Arch_AUR_Overlays = PackageManager + " -S goverlay-bin -y --needed --noconfirm"
+    def Arch_AUR_Overlays(self)
+        AUR_Overlays = PackageManager + " -S goverlay-bin -y --needed --noconfirm"
+        return AUR_Overlays
 
 # For installing Steam Tinker Lanuch
-    Arch_AUR_STL = PackageManager + " -S steamtinkerlaunch -y --needed --noconfirm"
+    def Arch_AUR_STL(self)
+        AUR_STL = PackageManager + " -S steamtinkerlaunch -y --needed --noconfirm"
+        return AUR_STL
 
     def __init__(self, PackageManager):
         self.PackageManager = PackageManager

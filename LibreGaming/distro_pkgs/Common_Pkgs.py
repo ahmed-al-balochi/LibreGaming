a8a22786963f6e744a7fcb4bcf5d664abf1a8a8b
Ahmed Al Balochi <a7mad98.work@gmail.com>
2022-03-29 14:37:48 +0500

    added whoami() to check for root usage

--- LibreGaming/distro_pkgs/Common_Pkgs.py
+++ LibreGaming/distro_pkgs/Common_Pkgs.py
@@ -5,20 +5,24 @@ class Common_Pkgs:
     Here are all the pkgs that are distro agnostic
     """
 
+    # TO check if LibreGaming is executed with root or not 
+    def whoami(self, authorize):
+        whoami = str(subprocess.getoutput("whoami"))
+        if whoami == "root" and authorize == False:
+           print("Please run LibreGaming without sudo or doas command for this flag")
+           exit(0)
+        elif whoami != "root" and authorize == True:
+           print("Please run LibreGaming with sudo or doas command for this flag")
+           exit(0)
+
     # installation for Itch.io store
     def itch(self):
-        whoami = str(subprocess.getoutput("whoami"))
-        if whoami == "root":
-            print("Please run LibreGaming without the sudo or doas command to install itch correctly")
-        else:
-            print('Downloading itch.io')
-            os.system("wget 'https://itch.io/app/download?platform=linux' -O itch-setup")
-            os.system("chmod +x itch-setup && ./itch-setup")
+        self.whoami(False)
+        print('Downloading itch.io')
+        os.system("wget 'https://itch.io/app/download?platform=linux' -O itch-setup")
+        os.system("chmod +x itch-setup && ./itch-setup")
 
     # installation for Athenaeum store For FOSS games
     def Athenaeum(self):
-        whoami = str(subprocess.getoutput("whoami"))
-        if whoami == "root":
-            print("Please run LibreGaming without the sudo or doas command to install Athenaeum correctly")
-        else:
-            os.system("flatpak install flathub com.gitlab.librebob.Athenaeum -y")
+        self.whoami(False)
+        os.system("flatpak install flathub com.gitlab.librebob.Athenaeum -y")
